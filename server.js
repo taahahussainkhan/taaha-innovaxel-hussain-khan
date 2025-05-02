afc9ceba24ddeb427f70788f4fb4e1b582af42ca
Taaha hussain Khan <taaha128@gmail.com>
2025-05-02 21:31:20 +0500

    UpdateUrl endpoint

--- server.js
+++ server.js
@@ -161,3 +161,46 @@ app.get('/shorten/stats/:shortCode', async (req, res) => {
     res.status(500).json({ message: 'Server Error', error });
   }
 });
+
+
+
+app.put('/shorten/:shortCode', async (req, res) => {
+  const { shortCode } = req.params;
+  const { url } = req.body;
+
+  if (!url || !isValidUrl(url)) {
+    return res.status(400).json({ message: 'Valid URL is required' });
+  }
+
+  try {
+
+    const urlRecord = await Url.findOne({ shortCode });
+
+    if (!urlRecord) {
+      return res.status(404).json({ message: 'Short URL not found' });
+    }
+
+
+    urlRecord.url = url;
+    urlRecord.updatedAt = new Date().toISOString();
+
+  
+    await urlRecord.save();
+
+    res.status(200).json({
+      id: urlRecord._id,
+      url: urlRecord.url,
+      shortCode: urlRecord.shortCode,
+      createdAt: urlRecord.createdAt,
+      updatedAt: urlRecord.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error });
+  }
+});
+
+function isValidUrl(url) {
+  const regex = /^(https?:\/\/)?([\w-]+(\.[\w-]+)+)(:\d+)?(\/[\w-]+)*(\?[\w-]+=[\w-]+(&[\w-]+=[\w-]+)*)?$/;
+  return regex.test(url);
+}
+
